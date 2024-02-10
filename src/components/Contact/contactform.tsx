19debfe3203c610853ea8b8cdfb5fda57a4944f4
flush286 <112771274+flush286@users.noreply.github.com>
2024-02-10 20:10:16 +0100

    fix the css for header  and hero.

--- src/components/Contact/contactform.tsx
+++ src/components/Contact/contactform.tsx
@@ -1,60 +1,58 @@
-import {IoIosSend} from "react-icons/io";
+import { IoIosSend } from "react-icons/io";
 import React from "react";
 
 function ContactForm() {
-    return (
-        <div className="max-w-screen-xl my-16 px-4 md:px-8 lg:px-16 xl:px-32 mx-auto h-auto w-auto">
-            <div className="grid gap-8 grid-cols-1 md:grid-cols-2 bg-stone-50 text-gray-900 rounded-lg shadow-lg">
-                <div className="p-8">
-                    <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight mb-4">
-                        Contactez-nous
-                    </h2>
-                    <div className="text-gray-700">
-                        Nous nous engageons à répondre en moins de{" "}
-                        <span className="underline">48 heures</span>.
-                    </div>
-                </div>
-                <div className="p-8">
-                    <div className="mb-4">
+  return (
+    <div className="max-w-screen-xl flex justify-center items-center px-4 md:px-8 lg:px-16 xl:px-32 mx-auto h-[78vh] w-auto">
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-2 bg-stone-50 text-gray-900 rounded-lg shadow-lg">
+        <div className="p-8">
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight mb-4">
+            Contactez-nous
+          </h2>
+          <div className="text-gray-700">
+            Nous nous engageons à répondre en moins de{" "}
+            <span className="underline">48 heures</span>.
+          </div>
+        </div>
+        <div className="p-8">
+          <div className="mb-4">
             <span className="uppercase text-sm text-gray-600 font-bold">
               Nom / Société
             </span>
-                        <input
-                            className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="text"
-                            placeholder=""
-                        />
-                    </div>
-                    <div className="mb-4">
+            <input
+              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder=""
+            />
+          </div>
+          <div className="mb-4">
             <span className="uppercase text-sm text-gray-600 font-bold">
               Adresse mail
             </span>
-                        <input
-                            className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="text"
-                        />
-                    </div>
-                    <div className="mb-4">
+            <input
+              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+              type="text"
+            />
+          </div>
+          <div className="mb-4">
             <span className="uppercase text-sm text-gray-600 font-bold">
               Message
             </span>
-                        <textarea
-                            className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                        ></textarea>
-                    </div>
-                    <div>
-                        <a
-                            className="inline-flex font-mono font-bold justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 text-white text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 "
-                            href="#"
-                        >
-                            Envoyer
-                            <IoIosSend size={18} />
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <textarea className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+          </div>
+          <div>
+            <a
+              className="inline-flex font-mono font-bold justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 text-white text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 "
+              href="#"
+            >
+              Envoyer
+              <IoIosSend size={18} />
+            </a>
+          </div>
         </div>
-        )
+      </div>
+    </div>
+  );
 }
 
 export default ContactForm;
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
 
   return (
     <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:w-full before:h-full before:-z-[1] before:transform before:-translate-x-1/2dark">
-      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
         <div className="mt-5 max-w-2xl text-center mx-auto">
           <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl ">
             Holium
