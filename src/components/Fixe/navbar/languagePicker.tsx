5c47f1189bc7d5a2e793192c0613816392d8be1c
Saltiplouf <120452681+SaItiplouf@users.noreply.github.com>
2024-05-08 17:05:31 +0200

    fix slider phone

--- src/components/Fixe/navbar/languagePicker.tsx
+++ src/components/Fixe/navbar/languagePicker.tsx
@@ -20,7 +20,7 @@ export default function LanguagePicker() {
 
   return (
     <details className={`relative inline-block text-left mr-4`}>
-      <summary className="cursor-pointer inline-flex justify-center items-center w-full p-2 text-gray-700 hover:bg-gray-200 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+      <summary className="cursor-pointer slider-fix inline-flex justify-center items-center w-full p-2 text-gray-700 hover:bg-gray-200 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
