3fda49e72eb0dd9beea72e3ccc8c4b17b187194b
Saltiplouf <120452681+SaItiplouf@users.noreply.github.com>
2024-05-08 16:08:35 +0200

    detection i18n language, meta & favico

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,31 +1,52 @@
-import i18n from "i18next";
+import i18n, { InitOptions } from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import LanguageDetector, {
+  DetectorOptions,
+} from "i18next-browser-languagedetector";
 import tradFr from "./tradFr.json";
 import tradEn from "./tradEn.json";
 
-i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    debug: true,
-    fallbackLng: "fr",
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+const detectionOptions: DetectorOptions = {
+  order: [
+    "querystring",
+    "cookie",
+    "localStorage",
+    "sessionStorage",
+    "navigator",
+    "htmlTag",
+    "path",
+    "subdomain",
+  ],
+  lookupQuerystring: "lng",
+  lookupCookie: "i18next",
+  lookupLocalStorage: "i18nextLng",
+  lookupSessionStorage: "i18nextLng",
+  lookupFromPathIndex: 0,
+  lookupFromSubdomainIndex: 0,
+  caches: ["localStorage", "cookie"],
+  excludeCacheFor: ["cimode"],
+  cookieMinutes: 10,
+  cookieDomain: "myDomain",
+  cookieOptions: { path: "/", sameSite: "strict" }, // Assurez-vous que sameSite est correctement défini
+};
+
+const i18nOptions: InitOptions = {
+  debug: true,
+  fallbackLng: "fr",
+  detection: detectionOptions,
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    fr: {
+      translation: tradFr,
     },
-    resources: {
-      fr: {
-        translation: tradFr,
-      },
-      en: {
-        translation: tradEn,
-      },
+    en: {
+      translation: tradEn,
     },
-  });
+  },
+};
+
+i18n.use(LanguageDetector).use(initReactI18next).init(i18nOptions);
 
 export default i18n;
